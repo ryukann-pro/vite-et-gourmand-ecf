f2aaedc4a84aa312fc5f8f082a4bf478a624ae5f
yoann lamouille <yoannlamouille@gmail.com>
2026-05-07 08:09:09 +0200

    Revert "Création du header"

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,7 +1,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/vite-et-gourmand-ecf/public/assets/js/script.js"></script>
 </body>
-<footer>
-  <p>TEST</p>
-</footer>
 </html>
