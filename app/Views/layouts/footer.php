0791381421db60b70a04f12bb19def5add8b2214
ryukann-pro <yoannlamouille@gmail.com>
2026-05-09 17:20:35 +0200

    Résolution conflit merge footer

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <footer class="site-footer py-4">
   <div class="container text-center">
 
@@ -39,9 +38,7 @@
 
   </div>
 </footer>
-=======
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/vite-et-gourmand-ecf/public/assets/js/script.js"></script>
 </body>
 </html>
->>>>>>> develop
