9a7989f9d34e26799ba76cbdf76956e9c93dfdfd
ryukann-pro <yoannlamouille@gmail.com>
2026-05-12 16:03:07 +0200

    Création de la page de connexion statique

---
+++ app/Views/pages/login.php
@@ -0,0 +1,67 @@
+<?php require_once __DIR__ . '/../layouts/header.php'; ?>
+
+<main class="login-page py-5">
+
+    <section class="container">
+
+        <div class="login-card">
+
+            <h1 class="login-title mb-5">
+                Connexion
+            </h1>
+
+            <form>
+
+                <div class="mb-4">
+                    <label for="email" class="form-label">
+                        Adresse email
+                    </label>
+
+                    <input
+                        type="email"
+                        id="email"
+                        class="form-control"
+                        placeholder="Votre adresse email"
+                    >
+                </div>
+
+                <div class="mb-2">
+                    <label for="password" class="form-label">
+                        Mot de passe
+                    </label>
+
+                    <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        placeholder="Votre mot de passe"
+                    >
+                </div>
+
+                <div class="text-end mb-4 text-center">
+                    <a href="#" class="forgot-password-link">
+                        Mot de passe oublié ?
+                    </a>
+                </div>
+
+                <button type="submit" class="btn login-btn w-100 mb-4">
+                    Se connecter
+                </button>
+
+                <p class="register-text mb-0">
+                    Vous n’avez pas de compte ?
+
+                    <a href="index.php?url=inscription" class="register-link">
+                        Créer un compte
+                    </a>
+                </p>
+
+            </form>
+
+        </div>
+
+    </section>
+
+</main>
+
+<?php require_once __DIR__ . '/../layouts/footer.php'; ?>
