a05fbd7eb9a2c2961f6bac06cf0f46e3504ac33a
yoann lamouille <yoannlamouille@gmail.com>
2026-08-11 12:01:01 +0200

    Ajout des notifications email avec Brevo

--- config/config.php
+++ config/config.php
@@ -1,3 +1,11 @@
 <?php
 
-define('BASE_URL', getenv('APP_BASE_URL') ?: '/vite-et-gourmand-ecf/public');
+define(
+    'BASE_URL',
+    getenv('APP_BASE_URL') ?: '/vite-et-gourmand-ecf/public'
+);
+
+define(
+    'APP_URL',
+    getenv('APP_URL') ?: 'http://localhost/vite-et-gourmand-ecf/public'
+);
